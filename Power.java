ec08a0898f61d958cbcea2d6aa775b5114074c7c
Rishabh Singh Bhadauriya <rishabh.2426mca335@kiet.edu>
2025-08-26 00:01:09 +0530

    power of X^y

---
+++ Power.java
@@ -0,0 +1,14 @@
+public class Power
+{
+    public static void main(String[] args){
+        int result=1;
+        int x=2;
+        int y=4;
+        for(int i=1;i<=y;i++){
+            result= result*x;
+         
+        }
+           System.out.println(result);
+        
+    }
+}
